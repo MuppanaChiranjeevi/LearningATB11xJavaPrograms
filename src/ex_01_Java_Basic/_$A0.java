fb89ac8a25f5f3b12429d152592da374b5159ed6
MuppanaChiranjeevi <chiranjeevimuppana982@gmail.com>
2025-03-11 20:07:28 +0530

    11 march 2025

---
+++ src/ex_01_Java_Basic/_$A0.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class _$A0 {
+    public static void main(String[] args) {
+        System.out.println("_$A0 as class name");
+    }
+}
---
+++ src/ex_01_Java_Basic/we_can_have_class_name_as_big_as_we_want_as_per_convention.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class we_can_have_class_name_as_big_as_we_want_as_per_convention {
+    public static void main(String[] args) {
+        System.out.println("no length restriction for identitfiers");
+    }
+}
