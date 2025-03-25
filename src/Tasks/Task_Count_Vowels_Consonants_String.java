d3e7921f9b2e229cb72637241b085111e06513b8
MuppanaChiranjeevi <chiranjeevimuppana982@gmail.com>
2025-03-25 08:03:43 +0530

    25 march 2025

--- src/Tasks/Task_Count_Vowels_Consonants_String.java
+++ src/Tasks/Task_Count_Vowels_Consonants_String.java
@@ -2,21 +2,26 @@ package Tasks;
 
 import java.util.Scanner;
 
-public class Task_Count_Vowels_Consonants_String {
-    public static void main(String[] args) {
+public class Task_Count_Vowels_Consonants_String
+{
+    public static void main(String[] args)
+    {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the string: ");
         String inputString = scanner.nextLine();
         inputString=inputString.toLowerCase();
         int vowels=0,consosnants=0;
 
-        for (int i=0;i<inputString.length();i++){
+        for (int i=0;i<inputString.length();i++)
+        {
             char ch=inputString.charAt(i);
             if(ch >= 'a' && ch <= 'z'){ // to Check if it's a letter
-                if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                {
                     vowels++;
                 }
-                else {
+                else
+                {
                     consosnants++;
                 }
             }
--- src/Tasks/Task_Palindrome.java
+++ src/Tasks/Task_Palindrome.java
@@ -6,22 +6,23 @@ public class Task_Palindrome {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         System.out.print("Enter the string: ");
-        String str=scanner.nextLine().toLowerCase();
-        // Remove non-alphabetic characters and spaces
-        str = str.replaceAll("[^a-zA-Z]", "");
-        boolean flag = true;
-        for (int i=0;i<str.length()/2;i++) {
-            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
-                flag = false;
-                break;
-            }
-        }
-        if (flag) {
+        String inputStr=scanner.next().toLowerCase();
+        String reversedString=reverseString(inputStr); // calling reverseString method
+
+        if (inputStr.equals(reversedString)) {
             System.out.println("Palindrome");
         }
         else {
             System.out.println("Not a Palindrome");
             }
         }
+
+    private static String reverseString(String inputStr) {
+        String revString="";
+        for (int i=inputStr.length()-1;i>=0;i--){
+            revString=revString + inputStr.charAt(i);
+        }
+        return revString;
     }
+}
 
